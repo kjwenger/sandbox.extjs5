79591493aa30e5e1783ea89bc8a95b8f2d5d1a54
kjwenger <kjwenger@yahoo.com>
2015-11-12 22:05:27 -0500

    initial load

---
+++ app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('sandbox_extjs5.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'sandbox_extjs5'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
